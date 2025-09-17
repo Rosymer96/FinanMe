e8111705f99d24fcfae867fed684186ef4c4f2c3
Rosa Vela <rosymer96@gmail.com>
2025-09-17 12:53:23 +0200

    Form creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ExpensesFormComponent } from './components/expenses-form/expenses-form.
 import { ExpenseListComponent } from './components/expense-list/expense-list.component';
 import { ExpensesChartComponent } from './components/expenses-chart/expenses-chart.component';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
 
 export const routes: Routes = [
-  { path: '', component: ExpensesChartComponent },
+  { path: '', component: LoginUserComponent},
   { path: 'anadir-gasto', component: ExpensesFormComponent },
   { path: 'gastos-lista', component: ExpenseListComponent },
+  { path: 'gastos', component: ExpenseListComponent },
   { path: 'register', component: RegisterUserComponent },
 ];
--- src/app/components/login-user/login-user.component.ts
+++ src/app/components/login-user/login-user.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: 'app-login-user',
-  imports: [],
+  imports: [MatInputModule, ReactiveFormsModule],
   templateUrl: './login-user.component.html',
-  styleUrl: './login-user.component.scss'
+  styleUrl: './login-user.component.scss',
 })
 export class LoginUserComponent {
+  errorMessage: string = '';
+  messageResponse: string = '';
 
+  public form = new FormGroup({
+    email: new FormControl<string | null>(null, [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl<string | null>(null, [Validators.required]),
+  });
+
+  onLogin(){
+
+  }
 }
