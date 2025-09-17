370f45f185b4105afcf03b0cbbdd5636ee068d55
Rosa Vela <rosymer96@gmail.com>
2025-09-17 12:47:51 +0200

    Html de login

---
+++ src/app/components/login-user/login-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-user',
+  imports: [],
+  templateUrl: './login-user.component.html',
+  styleUrl: './login-user.component.scss'
+})
+export class LoginUserComponent {
+
+}
